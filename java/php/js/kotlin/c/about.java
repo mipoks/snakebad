efb329fed4ac86a236513db2359583e63645cf84
mipoks <d_zakiev@mail.ru>
2020-05-20 21:17:21 +0300

    Delete info

---
+++ java/php/js/kotlin/c/about.java
@@ -0,0 +1,9 @@
+
+public class Mama {
+
+    public static void main(String[] args) {
+        System.out.println("But every programmer has a different penmanship");
+        System.out.println("by mipoks");
+        System.exit(1);
+        }
+}
